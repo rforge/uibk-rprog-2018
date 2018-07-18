fa734bbe9000d5d4729ecd83fb082978d7e5c3cf
mlang <mlang@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-07-18 13:47:42 +0000

    Update plot functions and include cool polygons
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uibk-rprog-2018@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/circmax/R/circtree_plot.R
+++ pkg/circmax/R/circtree_plot.R
@@ -14,40 +14,67 @@ plot.circtree <- function(x, terminal_panel = node_circular,
 }
 
 
-plot_circular <- function(X, coefs = NULL, stack = 2, cex = 0.8, label = TRUE, 
-  circlab = c('$0$', '$\\pi/2$', '$\\pi$','$3/2\\pi$'), fit_dens = FALSE){
+plot_circular <- function(X, coefs = NULL, stack = 10, cex = 0.8, label = TRUE, 
+  circlab = c('$0$', '$\\pi/2$', '$\\pi$','$3/2\\pi$'), polygons = TRUE, rug = TRUE, kernel_density = FALSE){
 
+  # Empty Plot
   par(mar = c(0, 0, 0, 0) + 0., cex = cex)
-
-  circ_ln <- seq(0, 360, 0.5) * pi / 180
-  plot(cos(circ_ln), sin(circ_ln), xlim = c(-2,2), ylim = c(-2,2),
+  #tmp <- par("pin")[1]/6
+  #par(cex = tmp)
+  plot(NA, xlim = c(-2,2), ylim = c(-2,2),
     type = "l", axes = FALSE, xlab = "",ylab = "", asp = 1)
+
+  # Histogram
+  Xhist <- hist(X, plot = FALSE, breaks = seq(0, 360, stack) * pi / 180)
+  idx <- (Xhist$density != 0)
+  Xscaled <- Xhist$density / max(Xhist$density)
+ 
+  # Draw densities (lines or polygons)
+  if (! polygons) {
+    segments(cos(Xhist$mids[idx]), sin(Xhist$mids[idx]),
+             (Xscaled[idx] + 1) * cos(Xhist$mids[idx]),
+             (Xscaled[idx] + 1) * sin(Xhist$mids[idx]))
+    #points(1 * cos(Xhist$mids), 1 * sin(Xhist$mids), pch = 20, col = c(NA, gray(0.2, alpha = 0.7))[idx + 1]) 
+  } else {
+    segwidth <- diff(Xhist$mids[1:2])     # width of the segment
+    segn     <- ceiling(segwidth / 0.005) # level of detail for the curvature of the segment
+    for (i in which(idx)) {
+      x <- seq(Xhist$mids[i] - segwidth/2, Xhist$mids[i] + segwidth/2, length = segn)
+      polygon(c(cos(x), (Xscaled[i] + 1) * cos(rev(x))),
+              c(sin(x), (Xscaled[i] + 1) * sin(rev(x))), border = NA, col = "gray80")
+    }
+  } 
+
+  # Plot rugs
+  if(rug){
+    segments(cos(X), sin(X),
+             (0.9) * cos(X),
+             (0.9) * sin(X), gray(0.2, alpha = 0.7))
+  }
+
+  # Plot circle
+  circ_ln <- seq(0, 360, 0.5) * pi / 180
+  lines(cos(circ_ln), sin(circ_ln))
   
+  # Plot ticks and labels
   circ_sgm <- seq(0, 360, 45) * pi / 180
-  segments(.9 * cos(circ_sgm), .9 * sin(circ_sgm), 0.99 * cos(circ_sgm), 0.99 * sin(circ_sgm))
+  segments(.8 * cos(circ_sgm), .8 * sin(circ_sgm), 1 * cos(circ_sgm), 1 * sin(circ_sgm))
 
   if(label & requireNamespace("latex2exp", quietly = TRUE)){
-    text(.7, 0, latex2exp::TeX(circlab[1]))
-    text(0, .7, latex2exp::TeX(circlab[2]))
-    text(-.7, 0, latex2exp::TeX(circlab[3]))
-    text(0, -.7, latex2exp::TeX(circlab[4]))
+    text(.65, 0, latex2exp::TeX(circlab[1]))
+    text(0, .65, latex2exp::TeX(circlab[2]))
+    text(-.65, 0, latex2exp::TeX(circlab[3]))
+    text(0, -.65, latex2exp::TeX(circlab[4]))
   }
 
-  Xhist <- hist(X, plot = FALSE, breaks = seq(0, 360, stack) * pi / 180)
 
-  idx <- (Xhist$density != 0)
-  points(1 * cos(Xhist$mids), 1 * sin(Xhist$mids), pch = 20, col = c(NA, gray(0.2, alpha = 0.7))[idx + 1]) 
-  
-  Xscaled <- Xhist$density / max(Xhist$density)
-  for(i in seq_along(Xhist$mids)) {
-    lines(c(1 * cos(Xhist$mids[i]), (Xscaled[i] + 1) * cos(Xhist$mids[i])), 
-      c(1 * sin(Xhist$mids[i]), (Xscaled[i] + 1) * sin(Xhist$mids[i])), col = gray(0.5))
-  }
-  
-  if(fit_dens){  
+  # Plot kernel density
+  if(kernel_density){  
     d <- circular::density.circular(circular::circular(X), bw = 1)
     lines(d, col = 2, lty = 2, lwd = 1.5)
   }
+
+  # Plot fitted density
   if(!is.null(coefs)){
     arrows(0, 0, 0.5 * cos(coefs[1]), 0.5 * sin(coefs[1]), col = 2, length = 0.1, code = 2, lwd = 2)
     circular::plot.function.circular(function(x) 
@@ -84,8 +111,8 @@ node_circular <- function(obj, which = NULL, id = TRUE, pop = TRUE,
 
     ## set up viewport tree
     top.vp <- grid::viewport(layout = grid::grid.layout(ncol = 1, nrow = 3,
-		       widths = grid::unit(c(1, 1, 1), c("null", "null", "null")), heights = grid::unit(c(0.3, 0.1, 0.6), c("npc", "npc", "npc"))),
-		       width = grid::unit(1, "npc"), height = grid::unit(1, "npc"), name = paste("node", nid, sep = ""))
+             widths = grid::unit(c(1, 1, 1), c("null", "null", "null")), heights = grid::unit(c(0.3, 0.1, 0.6), c("npc", "npc", "npc"))),
+             width = grid::unit(1, "npc"), height = grid::unit(1, "npc"), name = paste("node", nid, sep = ""))
     topmargin <- grid::viewport(layout.pos.col = 1, layout.pos.row = 1, name = "topmargin")
     text <- grid::viewport(layout.pos.col = 1, layout.pos.row = 2, name = "text")
     plot <- grid::viewport(layout.pos.col = 1, layout.pos.row = 3, name = "plot", )
@@ -98,10 +125,10 @@ node_circular <- function(obj, which = NULL, id = TRUE, pop = TRUE,
 
     if (is.null(mainlab)) { 
       mainlab <- if(id) {
-    	function(id, nobs, mu, kappa) sprintf("Node %s (n = %s) \n mu = %s, kappa = %s", id, nobs, mu, kappa)
-    	#function(id, nobs, mu, kappa) sprintf("Node %s (n = %s)", id, nobs)
+      function(id, nobs, mu, kappa) sprintf("Node %s (n = %s) \n mu = %s, kappa = %s", id, nobs, mu, kappa)
+      #function(id, nobs, mu, kappa) sprintf("Node %s (n = %s)", id, nobs)
       } else {
-    	function(id, nobs) sprintf("n = %s", nobs)
+      function(id, nobs) sprintf("n = %s", nobs)
       }
     }
     if (is.function(mainlab)) {
@@ -116,7 +143,7 @@ node_circular <- function(obj, which = NULL, id = TRUE, pop = TRUE,
     gridGraphics::grid.echo(function() plot_circular(y, coefs, ...), newpage = FALSE)
 
     if(ylab != "") grid::grid.text(ylab, y = grid::unit(0.5, "npc"), x = grid::unit(-2.5, "lines"), rot = 90)
-    if(xlab != "") grid::grid.text(xlab, x = grid::unit(0.5, "npc"), y = grid::unit(-2, "lines")) 	       
+    if(xlab != "") grid::grid.text(xlab, x = grid::unit(0.5, "npc"), y = grid::unit(-2, "lines"))         
 
     if(pop) grid::popViewport() else grid::upViewport()
     if(pop) grid::popViewport() else grid::upViewport()
