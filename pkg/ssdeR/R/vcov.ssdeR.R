9a845b022a7a331fc188673eea881e4ed7855a35
michaelb <michaelb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-09-04 08:02:33 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uibk-rprog-2018@107 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ssdeR/R/vcov.ssdeR.R
+++
@@ -1,9 +0,0 @@
-vcov.ssdeR <- function (object, ...)
-{
-  vc <- object$vcov
-  k <- length(object$coefficients)
-  vc[seq.int(length.out = k), seq.int(length.out = k),
-     drop = FALSE]
-  colnames(vc) <- rownames(vc) <- names(object$coefficients)
-  vc
-}
